bea0d874bdaaaf5c60d30b9d5d1d3b39aa58f57e
roybuffinga <75253262+roybuffinga@users.noreply.github.com>
2020-12-07 12:19:07 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    if (sprite.get(LedSpriteProperty.X) == 2) {
+        game.addScore(1)
+    } else {
+        game.gameOver()
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    sprite = game.createSprite(2, 2)
+})
+let sprite: game.LedSprite = null
+sprite = game.createSprite(2, 2)
+basic.forever(function () {
+    sprite.move(1)
+    sprite.ifOnEdgeBounce()
+    basic.pause(200)
+})
